af42b983bf1f13304072dc193d22f702f325ce87
andrewjmeng@gmail.com <andrewjmeng@gmail.com>
2017-04-10 15:20:56 -0700

    part one complete

---
+++ part one/main.js
@@ -0,0 +1,34 @@
+const table_data = [ { first_name : 'Rose',
+                     last_name  : 'Tyler',
+                     home       : 'Earth' },
+                   { first_name : 'Zoe',
+                     last_name  : 'Heriot',
+                     home       : 'Space Station W3'},
+                   { first_name : 'Jo',
+                     last_name  : 'Grant',
+                     home       : 'Earth'},
+                   { first_name : 'Leela',
+                     last_name  : null,
+                     home       : 'Unspecified'},
+                   { first_name : 'Romana',
+                     last_name  : null,
+                     home       : 'Gallifrey'},
+                   { first_name : 'Clara',
+                     last_name  : 'Oswald',
+                     home       : 'Earth'},
+                   { first_name : 'Adric',
+                     last_name  : null,
+                     home       : 'Alzarius'},
+                   { first_name : 'Susan',
+                     last_name  : 'Foreman',
+                     home       : 'Gallifrey'} ];
+
+$( document ).ready(function() {
+  table_data.forEach( function(item){
+    if(!item.last_name) {
+      item.last_name = ""
+    }
+    $(`<tr><td>${item.first_name}</td><td>${item.last_name}</td><td>${item.home}</td></tr>`).appendTo("#table_body")
+  })
+
+});
